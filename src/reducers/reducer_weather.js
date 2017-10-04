d4c1d313128602bbf445a7b74d383e36132b1763
Jakub Wasiljew <websolutionslab@gmail.com>
2017-10-05 00:53:02 +0200

    MiddleWare setUp. Creating a view.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,15 @@
-export default function ( state = null, action ) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function ( state = [], action ) {
     console.log('Action recived', action);
+    switch ( action.type ) {
+        case FETCH_WEATHER:
+            //creates new array that includes all old data and new data /? concat instead of push
+            // return state.concat([ action.payload.data ]);
+
+            //new array
+            return [ action.payload.data, ...state ];
+    }
+
     return state;
 }
