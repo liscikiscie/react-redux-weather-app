cc341aeecede27464b0ce9bba6b3ffafb8ad02e0
Jakub Wasiljew <websolutionslab@gmail.com>
2017-10-23 14:36:54 +0200

    GoogleMaps not working...

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class GoogleMap extends Component {
     componentDidMount() {
         // console.log(google.maps);
-        new google.maps.Map(this.refs.map, {
-            zoom: 12,
-            center: {
-                lat: this.props.lat,
-                lng: this.props.lon
-            }
-        })
+        // new google.maps.Map(this.refs.map, {
+        //     zoom: 12,
+        //     center: {
+        //         lat: this.props.lat,
+        //         lng: this.props.lon
+        //     }
+        // })
     }
 
     render() {
